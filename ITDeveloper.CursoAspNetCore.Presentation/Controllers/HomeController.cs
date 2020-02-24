9ca266c5b8bbccf42d97813a19b64e0192520b5f
Douglas Agenor <dougahasu@gmail.com>
2020-02-24 11:30:24 -0300

    Limpeza Solução

--- ITDeveloper.CursoAspNetCore.Presentation/Controllers/HomeController.cs
+++ ITDeveloper.CursoAspNetCore.Presentation/Controllers/HomeController.cs
@@ -1,19 +1,11 @@
 ﻿using ITDeveloper.CursoAspNetCore.Presentation.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace ITDeveloper.CursoAspNetCore.Presentation.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
